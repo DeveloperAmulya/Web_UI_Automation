b14a7562bf93b03ce6de180dd26e86c4c1b9e066
BattuAmulya <amulya.battu95@gmail.com>
2026-02-03 17:17:20 +0530

    Sprint 0 Day 2: Configure gitignore and clean test artifacts

--- src/test/java/com/tajhotels/automation/tests/SmokeTests.java
+++ src/test/java/com/tajhotels/automation/tests/SmokeTests.java
@@ -1,10 +1,24 @@
 package com.tajhotels.automation.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class SmokeTests {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    @Test
+    public void openTajHomePage_andVerifyTitle() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
 
-	}
+        driver.get("https://www.tajhotels.com/");
 
+        String title = driver.getTitle();
+        Assert.assertTrue(title != null && !title.isBlank(), "Page title should not be blank");
+
+        driver.quit();
+    }
 }
